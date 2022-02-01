095806500d7a67871cfd18c52476eaf1790f5aa0
Kaleb-Wang <80630440+Kaleb-Wang@users.noreply.github.com>
2022-01-31 20:14:10 -0800

    bug1fix

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -40,7 +40,9 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (markdown.charAt(nextOpenBracket - 1) != '!') {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
             System.out.println(currentIndex);
 
