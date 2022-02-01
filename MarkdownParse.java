3cacf32a47ffe648f43a8d671c5c2a19471ed3ae
Kaleb-Wang <80630440+Kaleb-Wang@users.noreply.github.com>
2022-01-31 20:07:05 -0800

    bug1fix

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -41,7 +41,8 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             // if (nextCloseBracket+1==openParen && (nextOpenBracket == 0 || markdown.charAt(nextOpenBracket - 1) != '!')) {
-            if (nextOpenBracket ==0 || markdown.charAt(nextOpenBracket - 1) != '!') {
+            // if (nextOpenBracket ==0 || markdown.charAt(nextOpenBracket - 1) != '!') {
+            if (markdown.charAt(nextOpenBracket - 1) != '!') {
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
             currentIndex = closeParen + 1;
